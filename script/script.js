6b78bf04936711053c0937c0ca4bbb2df4ba3b04
Tornike Maghradze <mt.georgian@gmail.com>
2021-08-29 15:55:39 +0400

    add js button click alert, burger toggle function, css smooth scroll, and meta tags for fb.

--- script/script.js
+++ script/script.js
@@ -7,3 +7,10 @@ document.getElementById('email').addEventListener('focus', function(){
 document.getElementById('sendButton').addEventListener('click', function(){
 	alert('Your Request Is Accepted.')
 })
+
+let navigationLeftBlock = document.getElementById('navigation');
+let burgerButton = document.getElementById('burger');
+
+burgerButton.addEventListener('click', function(){
+	navigationLeftBlock.classList.toggle('burgerBar');
+})
