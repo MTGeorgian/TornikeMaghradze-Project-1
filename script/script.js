a7cdd41821379fb06c797134247e1a11430752fe
Tornike Maghradze <mt.georgian@gmail.com>
2021-08-29 10:27:26 +0400

    add margin in style, remove few lines in html, add script.js whith three eventlistener.

---
+++ script/script.js
@@ -0,0 +1,9 @@
+document.getElementById('section-one-discover').addEventListener('click', function(){
+	alert('You Clicked Discover Button.')
+})
+document.getElementById('email').addEventListener('focus', function(){
+	console.log('Email Input Is Focused');
+})
+document.getElementById('sendButton').addEventListener('click', function(){
+	alert('Your Request Is Accepted.')
+})
